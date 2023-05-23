4651caa0bbe340f00baff1b7db4846efc86c2299
Hemasai Thaninki <69592038+Hemasai1344@users.noreply.github.com>
2023-05-23 18:21:08 +0530

    Add files via upload

---
+++ python_tutorial.py
@@ -0,0 +1,113 @@
+#=====================PYTHON====================#
+
+# Data Types
+"""
+integer = 5
+floating = 5.5
+string = "Hemasai"
+Boolean = True
+Nothing = None
+array = [1,2,3,4]
+tup = (1,2,3,4)
+sets = {1,2,3,4}
+dictinory = {'key':'value','key1':'value1'}
+"""
+
+#LISTS
+"""
+arr
+[4, 2, 5, 1, 6, 7, 3, 8]
+arr.append(9)
+arr
+[4, 2, 5, 1, 6, 7, 3, 8, 9]
+arr.pop()
+9
+arr
+[4, 2, 5, 1, 6, 7, 3, 8]
+arr.remove(8)
+arr
+[4, 2, 5, 1, 6, 7, 3]
+arr.sort()
+arr
+[1, 2, 3, 4, 5, 6, 7]
+arr.sort(reverse=True)
+arr
+[7, 6, 5, 4, 3, 2, 1]
+hemasai=arr.copy()
+hemasai
+[7, 6, 5, 4, 3, 2, 1]
+arr
+[7, 6, 5, 4, 3, 2, 1]
+del arr
+hemasai
+[7, 6, 5, 4, 3, 2, 1]
+s=hemasai
+s
+[7, 6, 5, 4, 3, 2, 1]
+del hemasai
+s
+[7, 6, 5, 4, 3, 2, 1]
+arr
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    arr
+NameError: name 'arr' is not defined
+s
+[7, 6, 5, 4, 3, 2, 1]
+max(s)
+7
+min(s)
+1
+sum(s)
+28
+s*2
+[7, 6, 5, 4, 3, 2, 1, 7, 6, 5, 4, 3, 2, 1]
+s.sort()
+s
+[1, 2, 3, 4, 5, 6, 7]
+s
+[1, 2, 3, 4, 5, 6, 7]
+k=[7,8]
+s.append(k)
+s
+[1, 2, 3, 4, 5, 6, 7, [7, 8]]
+s.extend(k)
+s
+[1, 2, 3, 4, 5, 6, 7, [7, 8], 7, 8]
+del s[7]
+s
+[1, 2, 3, 4, 5, 6, 7, 7, 8]
+s.remove(7)
+s
+[1, 2, 3, 4, 5, 6, 7, 8]
+arr=[4,2,5,1,6,7,3]
+#lets do some fun
+
+arr.append(8) #it will append the value in the last of the arr list
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
